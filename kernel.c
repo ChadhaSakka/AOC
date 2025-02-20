f135f802460ab7dc17554f1204e02774535812e9
Chadha-Sakka <chadha.sakka247@example.com>
2025-02-20 13:25:54 +0100

    New opt version inversion boucles

--- kernel.c
+++ kernel.c
@@ -16,17 +16,36 @@ void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
     }
 }
 
-#elif defined OPT2
+#elif defined (OPT2)
 #include <math.h>
+#include <omp.h>
 
 void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
     unsigned i, j, k;
     float log_x = log(x); // Calcul unique de log(x)
-    
     /* Amélioration de la localité mémoire en accédant à b[k][j] de manière plus contigue ]*/
     for (k = 0; k < 6; k++) {
         for (j = 0; j < n; j++) {
+        float bkj = b[k][j];
+        #pragma omp simd safelen(8) aligned(a, b: 32)
             for (i = 0; i < n; i++) {
+                a[i][j] += log_x * bkj;
+            }
+        }
+    }
+}
+
+#elif defined (OPT3)
+#include <math.h>
+#include <omp.h>
+void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
+    unsigned i, j, k;
+    float log_x = log(x); // Calcul unique de log(x)
+    /* Inversion de boucles */
+    for (k = 0; k < 6; k++) {
+        #pragma omp simd
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {       
                 a[i][j] += log_x * b[k][j];
             }
         }
