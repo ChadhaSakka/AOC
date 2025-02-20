8584bd1238cddd723e69aceda6714a8a66af5ac2
Chadha-Sakka <chadha.sakka247@example.com>
2025-02-20 12:50:02 +0100

    Adding version OPT2 for better memory locality

--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
 #if defined (OPT1)
 #include <math.h>
 #include <omp.h>
-/* Removing of store to load dependency (array ref replaced by scalar) */
+
 void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
     unsigned i, j, k;
     float log_x = log(x); // Calcul unique de log(x)
@@ -17,20 +17,22 @@ void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
 }
 
 #elif defined OPT2
+#include <math.h>
 
-#include <string.h> // memset
+void kernel(unsigned n, float a[n][n], const float b[n][n], float x) {
+    unsigned i, j, k;
+    float log_x = log(x); // Calcul unique de log(x)
+    
+    /* Amélioration de la localité mémoire en accédant à b[k][j] de manière plus contigue ]*/
+    for (k = 0; k < 6; k++) {
+        for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) {
+                a[i][j] += log_x * b[k][j];
+            }
+        }
+    }
+}
 
-/* ijk -> ikj permutation to make stride 1 the innermost loop */
-/*void kernel (unsigned n, float a[n][n], float b[n][n], float c[n][n]) {
-   int i, j, k;
-
-   memset (c, 0, n * n * sizeof c[0][0]);
-
-   for (i=0; i<n; i++)
-      for (k=0; k<n; k++)
-         for (j=0; j<n; j++)
-            c[i][j] += a[i][k] * b[k][j];
-}*/
 
 #else
 
